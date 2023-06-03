75dfccee36cd7a4b277fa5198366726f6bb9b55f
Denys <tymchenko.denys@gmail.com>
2023-06-03 13:46:24 +0300

    resizing is now working

--- main.js
+++ main.js
@@ -40,3 +40,14 @@ scene.add(camera);
 const renderer = new WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+window.addEventListener('resize', () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+});
